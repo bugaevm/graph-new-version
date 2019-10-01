8708dfc01438f5773a83b8dcb3ae4165ae5d1844
bugaevm <bugaev.ma@phystech.edu>
2019-10-01 13:04:18 +0300

    final

--- graph/codes/3/03.py
+++ graph/codes/3/03.py
@@ -24,7 +24,7 @@ class Object:
         self.content = args
         self.change = changing
 
-        objects.append(self)
+        objects.add(self)
 
     def remove(self):
         global objects
@@ -32,7 +32,7 @@ class Object:
         for el in self.content:
             deleteObject(el)
 
-        objects.pop(objects.index(self))
+        objects -= {self}
 
 def html_col(col):
     r, g, b = col
@@ -54,7 +54,7 @@ def change_col(col1, col2, i, length):
     return html_col((r, g, b))
 
 
-objects = list()
+objects = set()
 
 def create_background():
     global background
@@ -255,6 +255,182 @@ def animate_ufo(i):
         ufo.remove()
         create_ufo()
 
+def create_dodik():
+    global dodik
+
+    content = list()
+
+    body = (513, 761, 513 + 54, 761 + 117)
+    content.append(c.create_oval(*body, fill='#dde9af', outline='#dde9af'))
+
+    head = list()
+
+    head.append((500, 699))
+    head.append((499, 692))
+    head.append((499, 686))
+    head.append((500, 683))
+    head.append((502, 680))
+    head.append((507, 676))
+    head.append((521, 678))
+    head.append((541, 678))
+    head.append((569, 675))
+    head.append((580, 675))
+    head.append((592, 678))
+    head.append((605, 684))
+    head.append((607, 687))
+    head.append((607, 694))
+    head.append((605, 704))
+    head.append((599, 717))
+    head.append((593, 727))
+    head.append((584, 739))
+    head.append((564, 760))
+    head.append((556, 764))
+    head.append((546, 764))
+    head.append((536, 760))
+    head.append((523, 748))
+    head.append((518, 740))
+    head.append((504, 710))
+
+    head_outline = [(507, 709)] + head + [(496, 692)]
+
+
+    penColor('#dde9af')
+    brushColor('#dde9af')
+    content.append(polygon(head))
+
+    penColor('#090a06')
+    content.append(polyline(head_outline))
+
+
+    ovals = list()
+
+    ovals.append((473, 609, 473 + 27, 609 + 22))
+    ovals.append((479, 629, 479 + 21, 629 + 12))
+    ovals.append((491, 642, 491 + 17, 642 + 20))
+    ovals.append((502, 661, 502 + 10, 661 + 15))
+
+    ovals.append((647, 633, 647 + 23, 633 + 28))
+    ovals.append((626, 635, 626 + 15, 635 + 12))
+    ovals.append((607, 643, 607 + 15, 643 + 16))
+    ovals.append((602, 660, 602 + 8, 660 + 15))
+    ovals.append((592, 667, 592 + 18, 667 + 18))
+
+    ovals.append((481, 808, 481 + 11, 808 + 15))
+    ovals.append((490, 789, 490 + 23, 789 + 17))
+    ovals.append((501, 767, 501 + 28, 767 + 28))
+
+    ovals.append((602, 794, 602 + 32, 794 + 17))
+    ovals.append((575, 785, 575 + 27, 785 + 18))
+    ovals.append((561, 770, 561 + 29, 770 + 29))
+
+    ovals.append((471, 899, 471 + 28, 899 + 27))
+    ovals.append((494, 872, 494 + 23, 872 + 47))
+    ovals.append((498, 834, 498 + 29, 834 + 44))
+
+    ovals.append((547, 851, 547 + 29, 851 + 42))
+    ovals.append((558, 887, 558 + 21, 887 + 48))
+    ovals.append((575, 915, 575 + 28, 915 + 27))
+
+
+    for oval in ovals:
+        content.append(c.create_oval(*oval, fill='#dde9af', outline='#dde9af'))
+
+    dodik = Object(
+    c.create_oval(514, 691, 514 + 35, 691 + 33, fill='black', outline='black'),
+    c.create_oval(570, 698, 570 + 24, 698 + 26, fill='black', outline='black'),
+    *content)
+
+def create_eye1():
+    global eye1
+
+    obj = c.create_oval(531, 708, 531 + 9, 708 + 8, fill='white', outline='white')
+    eye1 = Object(obj)
+
+    eye1.pos1 = (xCoord(obj) + 1, yCoord(obj) + 1)
+    eye1.pos2 = (514 + (35 - 9) // 2, 691 + (33 - 8) // 2)
+    eye1.size = 9.5
+
+def create_eye2():
+    global eye2
+
+    obj = c.create_oval(581, 711, 581 + 7, 711 + 7, fill='white', outline='white')
+    eye2 = Object(obj)
+
+    eye2.pos1 = (xCoord(obj) + 1, yCoord(obj) + 1)
+    eye2.pos2 = (570 + (24 - 7) // 2, 698 + (26 - 7) // 2)
+    eye2.size = 7
+
+
+def create_apple():
+    global apple
+
+    content = list()
+    content.append(
+    c.create_oval(620, 751, 620 + 55, 751 + 53, fill='#c83737', outline='#c83737')
+    )
+
+    branch = list()
+
+    branch.append((645, 755))
+    branch.append((648, 751))
+    branch.append((650, 747))
+    branch.append((655, 741))
+    branch.append((662, 733))
+
+    content.append(polyline(branch))
+
+    leave = list()
+
+    leave.append((650, 748))
+    leave.append((644, 743))
+    leave.append((642, 738))
+    leave.append((642, 727))
+    leave.append((644, 727))
+    leave.append((648, 733))
+    leave.append((651, 739))
+    leave.append((651, 747))
+
+    brushColor('#88aa00')
+    content.append(polygon(leave))
+
+    apple = Object(*content, changing=animate_apple)
+
+va_const = -8
+g_const = 0.75
+
+va = 0
+g = 0
+def animate_apple(i):
+    global va, g
+
+    dy = va + g // 2
+    va += g
+
+    for obj in apple.content:
+        moveObjectBy(obj, 0, dy)
+
+
+    if i == step * 2:
+        va = va_const
+        g = g_const
+
+    sep = int(2 * abs(va_const / g_const) + 1)
+
+    if i == sep + step * 2:
+        va = 0
+        g = 0
+
+    if i == int(sep + 2.5 * step):
+        va = va_const
+        g = g_const
+
+    if i == int(2 * sep + 2.5 * step):
+        va = 0
+        g = 0
+
+        apple.remove()
+        create_apple()
+
 i = 1
 def animation():
     global i
@@ -271,8 +447,10 @@ create_sun()
 create_clouds()
 create_ray()
 create_ufo()
-# create_dodik()
-# create_apple()
+create_dodik()
+create_eye1()
+create_eye2()
+create_apple()
 
 onTimer(animation, int(fps * 1000 + 0.5))
 
